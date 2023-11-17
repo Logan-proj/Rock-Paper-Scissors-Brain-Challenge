56abe09a6dc22e14f677c7754cdda4848f55db03
Logan <logan0777@protonmail.com>
2023-11-17 06:15:40 -0500

    Initial Commit

---
+++ Rock-Paper-Scissors Brain Challenge/Rock_Paper_Scissors_Brain_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_Scissors_Brain_ChallengeApp.swift
+//  Rock-Paper-Scissors Brain Challenge
+//
+//  Created by Logan Toms on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_Scissors_Brain_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
